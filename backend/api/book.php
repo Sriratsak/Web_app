5dba2f01b8775a050c272dfcaa32fcd1ef546969
bas <batzazana@gmail.com>
2026-03-28 19:26:39 +0700

    data.php

--- backend/api/book.php
+++ backend/api/book.php
@@ -1,5 +1,8 @@
 <?php
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *"); 
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 require_once "../Database.php";
 require_once "../Model/Book.php";
