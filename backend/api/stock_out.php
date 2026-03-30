fd95fb9140896dd4d76c572e205eecc81afc1a62
bas <batzazana@gmail.com>
2026-03-31 01:46:36 +0700

    ฟฟ

--- backend/api/stock_out.php
+++ backend/api/stock_out.php
@@ -1,18 +1,18 @@
 <?php
 session_start();
 
-// 🔥 DEBUG ERROR
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-// 🔥 CORS
+
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-// 🔥 OPTIONS
+
 if ($_SERVER['REQUEST_METHOD'] === "OPTIONS") {
     http_response_code(200);
     exit;
@@ -27,13 +27,12 @@ $stock = new StockOut($db);
 $method = $_SERVER['REQUEST_METHOD'];
 $uri = $_SERVER['REQUEST_URI'];
 
-// 🔥 match id
+
 $id = null;
 if(preg_match('#stock_out.php/([0-9]+)$#', $uri, $m)){
     $id = $m[1];
 }
 
-// 🔥 กัน null
 $data = json_decode(file_get_contents("php://input"), true) ?? [];
 
 /* ------------------ GET ------------------ */
--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// 🔥 Admin pages
 import Dashboard_admin from "./admin/Dashboard_admin";
 import Products_admin from "./admin/Products_admin";
 import StockReport_admin from "./admin/StockReport_admin";
 // import Withdraw_admin from "./admin/Withdraw_admin";
 // import Receive_admin from "./admin_admin";
 import Users_admin from "./admin/Users_admin"; 
-
-// 🔥 User pages
 import Withdraw from "./pages/Withdraw";
 import Receive from "./pages/Receive";
-
-// 🔥 Auth
 import Login from "./com/Login";
 import Register from "./com/Register";
 
@@ -21,20 +15,18 @@ function App() {
     <BrowserRouter>
       <Routes>
 
-        {/* 🔐 Auth */}
+        {/* Auth */}
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
-        {/* 👑 Admin */}
+        {/* Admin */}
         <Route path="/dashboard_admin" element={<Dashboard_admin />} />
         <Route path="/products_admin" element={<Products_admin />} />
-        {/* <Route path="/withdraw_admin" element={<Withdraw_admin />} /> */}
-        {/* <Route path="_admin" element={<Receive_admin />} />?v */}
         <Route path="/users_admin" element={<Users_admin />} /> 
         <Route path="/StockReport_admin" element={<StockReport_admin />} /> 
 
-        {/* 👤 User */}
+        {/* User */}
         <Route path="/withdraw" element={<Withdraw />} />
         <Route path="/Receive" element={<Receive/>} />
 
